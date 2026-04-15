887288831a7841834310d7266a0505cde6d740f3
arthuzao22 <arthur88727771@gmail.com>
2026-04-14 23:12:30 -0300

    feat(migration): adicionar script de migração para remover valores padrão das colunas 'id'

---
+++ xua-delivery/prisma/migrations/20260415020627/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "19_cfg_banners" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "20_cfg_idempotency_keys" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "21_trn_payment_transactions" ALTER COLUMN "id" DROP DEFAULT;
